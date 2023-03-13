4d5430126e04b3dedadcd72010705d5c1d986158
D3Vitt1n <101475625+D3Vitt1n@users.noreply.github.com>
2023-03-13 13:42:35 -0300

    Corrigindo comentarios

--- Projeto calculadora/index.js
+++ Projeto calculadora/index.js
@@ -1,14 +1,14 @@
 // Criando função ao clicar no botão.
 function resultado(){
 
-    //Criação de 2 variveis para verificação do campo pegando somente numeros.
+    //Criação de 2 variveis para verificação do campo, pegando somente numeros.
     let campo1 = Number(document.getElementById('campo-um').value)
     let campo2 = Number(document.getElementById('campo-dois').value)
 
     //criando de variavel vazia
     total = 0
 
-    //Criando condição para selecionar as caixas quando estiver selecionada executar o calculo.
+    //Criando condição para selecionar as caixas quando estiver selecionada, executar o calculo.
     if(document.getElementById('caixa1').checked)
         total = campo1+campo2
     else if(document.getElementById('caixa2').checked)
@@ -23,7 +23,7 @@ function resultado(){
     document.getElementById('resultadoArea').innerHTML = String(total)
 
 
-    //Condição para verifação dos campos vazios.
+    //Condição para verificação dos campos vazios.
     if(campo1 == "" || campo2 == "" ){
         document.getElementById('resultadoArea').innerHTML = String('Preencha os Campos!')
     }
